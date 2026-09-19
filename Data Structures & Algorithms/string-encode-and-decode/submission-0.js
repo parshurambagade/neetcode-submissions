2aebd10a16a536ed1603d93e3e8822d802b58a75
Parshuram Bagade <parshurambagade855@gmail.com>
2026-09-19 13:40:32 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = "";
+
+        for(let str of strs){
+            result += `${str.length}#${str}`;
+        }
+
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let result = [];
+        let i = 0;
+
+        while(i<str.length){
+            let pos = str.indexOf("#", i);
+            let length = Number(str.slice(i, pos));
+            i = pos+1;
+            result.push(str.slice(i, i+length));
+            i += length;
+        }
+
+        return result;
+       
+    }
+}
