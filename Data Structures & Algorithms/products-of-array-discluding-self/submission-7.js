7c939ea8f6bb91746bf03e866719d506005a8741
Parshuram Bagade <parshurambagade855@gmail.com>
2026-09-21 11:09:55 +0530

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let length = nums.length;
+        let result = new Array(length).fill(1);
+
+        // prefix 
+        for(let i=1; i<length; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
+
+        let suffix = 1;
+
+        for(let i=length-2; i>=0; i--){
+            suffix *= nums[i+1];
+            result[i] *= suffix;
+        }
+
+
+        return result;
+    }
+}
